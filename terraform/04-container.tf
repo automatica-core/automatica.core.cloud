ae9c23bcc6f67e99c935cbb721d143e481569337
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-06-21 21:43:47 +0200

    remove generic ports

--- terraform/04-container.tf
+++ terraform/04-container.tf
@@ -2,24 +2,6 @@ variable "exclusion" {
   default = [80,443, 7000, 7500]
 }
 
-locals {
-  ports1 = [for i in range(1024,  2047) : {
-    port = i
-  } if !contains(var.exclusion, i)]
-  ports2 = [for i in range(2048,  3071) : {
-    port = i
-  } if !contains(var.exclusion, i)]
-  ports3 = [for i in range(3072,  4095) : {
-    port = i
-  } if !contains(var.exclusion, i)]
-  ports4 = [for i in range(4096,  5119) : {
-    port = i
-  } if !contains(var.exclusion, i)]
-  ports5 = [for i in range(5120,  6143) : {
-    port = i
-  } if !contains(var.exclusion, i)]
-}
-
 
 resource "azurerm_container_group" "frps" {
   name                = "${var.environment}-${var.prefix}-frps"
@@ -51,14 +33,12 @@ resource "azurerm_container_group" "frps" {
       port     = 7500
       protocol = "TCP"
     }
-    
-    dynamic "ports" {
-      for_each = local.ports1
-      content  {
-        port   = ports.value.port
-        protocol  = "UDP"
-      }
-    } 
+
+    ports {
+      port     = 3671
+      protocol = "UDP"
+    }
+
 
     # dynamic "ports" {
     #   for_each = local.ports2
