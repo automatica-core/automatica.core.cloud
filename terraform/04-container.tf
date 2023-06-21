539a5f2311910449f682f4a049d8b39f2b888132
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-06-21 20:34:51 +0200

    update tf

--- terraform/04-container.tf
+++ terraform/04-container.tf
@@ -21,12 +21,13 @@ locals {
 }
 
 
-resource "  " "frps" {
+resource "azurerm_container_group" "frps" {
   name                = "${var.environment}-${var.prefix}-frps"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
   restart_policy      = "Always"
+
 
   container {
     name   = "sync"
